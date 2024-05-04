9733117e0712d8ac1874acb0ef7ab34a1e991574
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-05-04 13:52:21 +0600

    axios baseURL updated

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default function axiosInstance() {
   const axiosInstance = axios.create({
-    // baseURL: 'https://pro-man-server.vercel.app',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://pro-man-server.vercel.app',
+    // baseURL: 'http://localhost:5000',
   })
 
   return axiosInstance;
